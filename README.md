24f02e79209e9da077cd5381ca6f31e7d8b3feab
AelanderFarias <aelanderf@gmail.com>
2022-09-20 01:47:38 -0300

    Primeiro Envio

---
+++ README.md
@@ -0,0 +1,28 @@
+# Projeto URNA ELETRÔNICA
+
+![preview](./gitHub/11111.png)
+
+## 🛠 Tecnologias
+
+* HTML
+* CSS
+* JavaScript
+
+
+## 🇧🇷 "Boca de urna" 
+> Vereador
+* 11111 - Galinha Pintadinha
+* 22222 - Galo Carijó
+>Prefeito
+* 33 - Borboletinha - Vice: Baratinha
+* 44 - Mariana - Vice: Pintinho
+
+![preview](./gitHub/VotoBranco.png)
+
+![preview](./gitHub/22222.png)
+
+![preview](./gitHub/33.png)
+
+![preview](./gitHub/44.png)
+
+![preview](./gitHub/fim.png)
---
+++ assets/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'Vereador',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Galinha Pintadinha',
+                partido: 'OVO',
+                fotos: [
+                    {url: 'profile-1.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'Galo Carijó',
+                partido: 'Bico',
+                fotos: [
+                    {url: 'profile-2.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Prefeito',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '33',
+                nome: 'Borboletinha',
+                partido: 'Asa',
+                vice: 'Baratinha',
+                fotos: [
+                    {url: 'profile-3.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'profile-4.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '44',
+                nome: 'Mariana',
+                partido: 'Conta',
+                vice: 'Pintinho',
+                fotos: [
+                    {url: 'profile-5.jpg', legenda: 'Prefeito', small: false},
+                    {url: 'profile-6.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
